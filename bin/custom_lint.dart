4cfbd8f773771659ae8f78668ad475bd0e18edc6
Biplab Dutta <biplabdutta860@gmail.com>
2022-07-17 10:30:22 +0545

    Changed StreamSubscription to StreamController

--- bin/custom_lint.dart
+++ bin/custom_lint.dart
@@ -4,19 +4,19 @@ import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
+// import 'package:linter/src/util/leak_detector_visitor.dart';
 
 void main(List<String> args, SendPort sendPort) {
   startPlugin(sendPort, _CustomLints());
 }
 
-bool _isStreamSubscription(DartType type) {
+bool _isStreamController(DartType type) {
   final element = type.element! as ClassElement;
   final source = element.librarySource.uri;
-  final isStreamSubscription = source.scheme == 'dart' &&
+  final isStreamController = source.scheme == 'dart' &&
       source.path == 'async' &&
-      element.name == 'StreamSubscription';
-  return isStreamSubscription ||
-      element.allSupertypes.any(_isStreamSubscription);
+      element.name == 'StreamController';
+  return isStreamController || element.allSupertypes.any(_isStreamController);
 }
 
 class _CustomLints extends PluginBase {
@@ -25,10 +25,25 @@ class _CustomLints extends PluginBase {
     final library = result.libraryElement;
     final classes = library.topLevelElements.whereType<ClassElement>();
     for (final classInstance in classes) {
-      final variable = classInstance.fields
+      final variables = classInstance.fields
           .whereType<VariableElement>()
-          .where((e) => _isStreamSubscription(e.type));
-      print(variable.first.name);
+          .where((e) => _isStreamController(e.type));
     }
+    yield Lint(
+      code: '',
+      message: '',
+      location: result.lintLocationFromOffset(42, length: 100),
+    );
   }
 }
+
+// class _Visitor extends LeakDetectorProcessors {
+//   static const _closeMethodName = 'close';
+
+//   @override
+//   Map<DartTypePredicate, String> get predicates => {
+//         _isStreamController: _closeMethodName,
+//       };
+
+//   _Visitor(super.rule);
+// }
