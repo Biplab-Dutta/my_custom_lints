d2c258ab2fe19f500028dbfdba91af1972d8a4c2
Biplab Dutta <biplabdutta860@gmail.com>
2022-07-17 19:00:09 +0545

    added necessary lint

--- bin/custom_lint.dart
+++ bin/custom_lint.dart
@@ -1,10 +1,13 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'dart:isolate';
 
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
+import 'package:analyzer_plugin/protocol/protocol_generated.dart';
+import 'package:analyzer_plugin/utilities/change_builder/change_builder_core.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
-// import 'package:linter/src/util/leak_detector_visitor.dart';
 
 void main(List<String> args, SendPort sendPort) {
   startPlugin(sendPort, _CustomLints());
@@ -27,23 +30,59 @@ class _CustomLints extends PluginBase {
     for (final classInstance in classes) {
       final variables = classInstance.fields
           .whereType<VariableElement>()
-          .where((e) => _isStreamController(e.type));
+          .where((e) => _isStreamController(e.type))
+          .toList();
+
+      final hasDisposeMethod = classInstance.getMethod('dispose') != null;
+
+      if (variables.isNotEmpty && !hasDisposeMethod) {
+        for (final variable in variables) {
+          final location = variable.nameLintLocation!;
+          // final a = classInstance.nameLintLocation!;
+
+          yield Lint(
+            severity: LintSeverity.warning,
+            code: 'close_stream_controllers',
+            message:
+                'StreamControllers should be closed in the dispose method. ',
+            correction: 'Add a dispose method',
+            location: result.lintLocationFromLines(
+              startLine: location.startLine,
+              endLine: location.endLine,
+              startColumn: location.startColumn,
+              endColumn: location.endColumn,
+            ),
+            getAnalysisErrorFixes: (lint) async* {
+              final filePath = library.source.fullName;
+              final changeBuilder = ChangeBuilder(session: result.session);
+              await changeBuilder.addDartFileEdit(
+                filePath,
+                (builder) {
+                  builder.addInsertion(
+                    1, // change this
+                    (builder) {
+                      builder.write(
+                        '''
+                      void dispose() {
+                          ${variable.name}.close();
+                        }''',
+                      );
+                    },
+                  );
+                },
+              );
+              final prioritizedSourceChange = PrioritizedSourceChange(
+                0,
+                changeBuilder.sourceChange..message = 'Add a dispose method',
+              );
+              yield AnalysisErrorFixes(
+                lint.asAnalysisError(),
+                fixes: [prioritizedSourceChange],
+              );
+            },
+          );
+        }
+      }
     }
-    yield Lint(
-      code: '',
-      message: '',
-      location: result.lintLocationFromOffset(42, length: 100),
-    );
   }
 }
-
-// class _Visitor extends LeakDetectorProcessors {
-//   static const _closeMethodName = 'close';
-
-//   @override
-//   Map<DartTypePredicate, String> get predicates => {
-//         _isStreamController: _closeMethodName,
-//       };
-
-//   _Visitor(super.rule);
-// }
